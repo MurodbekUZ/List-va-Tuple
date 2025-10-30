c12f61310c19b4f272613200a71e929d848bbfae
Azatov Murodbek <murodbekazatov126@gmail.com>
2025-10-30 15:35:08 +0500

    list va tuple

---
+++ list.py
@@ -0,0 +1,47 @@
+# 1-Masala: Mevalar ro'yxati
+# fruits = ['olma', 'banan', 'uzum']
+
+# fruits.append('nok')
+# print(f"Ro'yxatga 'nok' ni oxiriga qo'shing {fruits}")
+
+
+# uzun = len(fruits)
+# print(f"Ro'yxatning uzunligini toping {uzun}")
+
+# if "olma" in fruits:
+#     print(f" Ro'yxatda 'olma' mavjudligini tekshiring True")
+# else:
+#     print(f" Ro'yxatda 'olma' mavjudligini tekshiring False")
+
+# banan = fruits.index('banan')
+# print(f"Banan ning indeksini toping {banan}")
+
+# 2-Masala: Ranglar ro'yxati
+# colors = ['qizil', 'yashil', 'ko\'k']
+# colors.append( 'sariq')
+# print(f"Ro'yxatga 'sariq' qo'shing {colors}")
+
+# colors.sort()
+# print(f"Ro'yxatni alifbo tartibida tartiblang {colors}")
+
+# uzun = len(colors)
+# print(f"Ro'yxatning uzunligini toping {uzun}")
+
+# if "yashil" in colors:
+#     print(f"Ro'yxatda 'yashil' mavjudligini tekshiring: True")
+# else:
+#     print(f"Ro'yxatda 'yashil' mavjudligini tekshiring: False")
+
+# 3-Masala: Shaharlar
+cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+
+print(f"Ro'yxatga 'Xiva' qo'shing {cities}")
+
+samarqand = cities.index('Samarqand')
+print(f"Samarqand' ning indeksini toping {samarqand}")
+
+uzun = len(cities)
+print(f"Ro'yxatning uzunligini toping {uzun}")
+
+cities.sort()
+print(f"Ro'yxatni alifbo tartibida tartiblang {cities}")

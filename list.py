94034c8fedc8edb520a1d3647d71ed817cd368e9
Azatov Murodbek <murodbekazatov126@gmail.com>
2025-10-30 16:13:09 +0500

    yangilandi

--- list.py
+++ list.py
@@ -18,6 +18,7 @@
 
 # 2-Masala: Ranglar ro'yxati
 # colors = ['qizil', 'yashil', 'ko\'k']
+
 # colors.append( 'sariq')
 # print(f"Ro'yxatga 'sariq' qo'shing {colors}")
 
@@ -33,15 +34,104 @@
 #     print(f"Ro'yxatda 'yashil' mavjudligini tekshiring: False")
 
 # 3-Masala: Shaharlar
-cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+# cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+#
+# print(f"Ro'yxatga 'Xiva' qo'shing {cities}")
+#
+# samarqand = cities.index('Samarqand')
+# print(f"Samarqand' ning indeksini toping {samarqand}")
+#
+# uzun = len(cities)
+# print(f"Ro'yxatning uzunligini toping {uzun}")
+#
+# cities.sort()
+# print(f"Ro'yxatni alifbo tartibida tartiblang {cities}")
 
-print(f"Ro'yxatga 'Xiva' qo'shing {cities}")
+# 4-Masala: Sonlar
+# numbers = [5, 2, 8, 1]
+#
+# numbers.append(10)
+# print(f" Ro'yxatga 10 ni qo'shing {numbers}")
+#
+# numbers.sort()
+# print(f" Ro'yxatni o'sish tartibida tartiblang {numbers}")
+#
+# uzun = len(numbers)
+# print(f"Ro'yxatning uzunligini toping {uzun}")
+#
+# if 8 in numbers:
+#     print("True")
+# else:
+#     print("False")
 
-samarqand = cities.index('Samarqand')
-print(f"Samarqand' ning indeksini toping {samarqand}")
+# 5-Masala: Hayvonlar
+# animals = ['mushuk', 'it', 'qush']
+#
+# animals.append('baliq')
+# print(f"Ro'yxatga 'baliq' ni qo'shing {animals}")
+#
+# animals.sort()
+# print(f"Ro'yxatni alifbo tartibida tartiblang {animals}")
+#
+# a = animals.index('it')
+# print(f"'it' ning indeksini toping {a}")
+#
+# u = len(animals)
+# print(f"Ro'yxatning uzunligini toping {u}")
 
-uzun = len(cities)
-print(f"Ro'yxatning uzunligini toping {uzun}")
+# 6-Masala: Onlayn meva do'konidagi savat
+# fruits = ['olma', 'banan', 'uzum', 'nok']
+#
+# fruits.append('kivi')
+# print(f"Ro'yxatga 'kivi' ni oxiriga qo'shing {fruits}")
+#
+# fruits.remove('banan')
+# print(f"'banan' ni ro'yxatdan olib tashlang {fruits}")
+#
+# fruits.insert(2, 'shaftoli')
+# print(f"'nok' dan oldin 'shaftoli' qo'shing {fruits}")
+#
+# fruits.sort()
+# print(f"Ro'yxatni alifbo tartibida tartiblang {fruits}")
+#
+# u = len(fruits)
+# print(f"Ro'yxatning uzunligini toping {u}")
+#
+# a = fruits.index('olma')
+# print(f"'olma' ning indeksini toping {a}")
+#
+# if "kivi" in fruits:
+#     print(" Ro'yxatda 'kivi' mavjudligini tekshiring: True")
+# else:
+#     print(" Ro'yxatda 'kivi' mavjudligini tekshiring: False")
+#
+# fruits.sort(reverse=True)
+# print(f"Ro'yxatni teskari tartibda tartiblang {fruits}")
+#
+# fruits.append('ananas')
+# c = fruits.copy()
+# print(f"Ro'yxatni nusxalang (copy()) va nusxaga 'ananas' qo'shing {c}")
 
-cities.sort()
-print(f"Ro'yxatni alifbo tartibida tartiblang {cities}")
+# 7-Masala: Restoran menyusi
+menu = ['osh', "lag'mon", 'shashlik', 'somsa']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
